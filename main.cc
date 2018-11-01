8684478502b0eee685a66614fd40253ebcc1872c
maxxtepper <maxxtepper@gmail.com>
2018-11-01 00:26:44 -0700

    Transaction test success

--- main.cc
+++ main.cc
@@ -10,5 +10,22 @@ int main (int argc, char* argv[]) {
 	std::string nameG = user0.getName();
 	std::cout << nameG << std::endl;
 
+	int id = 0;
+	std::string type = "cash";
+	float value = 10;
+	std::string note = "paid 2 cups/day";
+
+	Transaction trans0(id,type,value,note);
+
+	int idG = trans0.getId();
+	std::string typeG = trans0.getType();
+	float valueG = trans0.getValue();
+	std::string noteG = trans0.getNote();
+
+	std::cout << idG << std::endl;
+	std::cout << typeG << std::endl;
+	std::cout << valueG << std::endl;
+	std::cout << noteG << std::endl;
+
 	return 0;
 }
